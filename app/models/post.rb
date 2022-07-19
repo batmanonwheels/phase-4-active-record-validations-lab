c9cbef7a4a48129ee7eab198bc141d2806574c80
Devin Garcia <devingarcia5423@gmail.com>
2022-07-19 17:35:30 -0400

    commit tree 001571d51c85954c08c1a6fa08841c945f3a416c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: ['Fiction', 'Non-Fiction']
+  validate :title_include
+
+  CLICKBAIT_PATTERNS = [
+    "Won't Believe",
+    "Secret",
+    "Top[number]",
+    "Guess"
+  ]
+
+  def title_include
+    unless title.blank? || CLICKBAIT_PATTERNS.any? {|p| title.match(p)}
+      errors.add(:title, "title not clickbait-y enough")
+    end
+  end
 end
